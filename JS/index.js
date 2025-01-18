af4e6704cccc4a2e666002ed03a4a2e2b212d3de
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-01-18 12:07:41 -0500

    Added switch functionality to Sign In/Up

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,30 @@
-console.log('Hello World');
-
 const signInUpButton=document.querySelector('#signInUpButton');
-
-
 
 signInUpButton.addEventListener('click',()=>{
     console.log('Button Clicked');
     document.querySelector('#overlay').style.display='block';
+});
+
+
+
+
+const switchButton=document.querySelector('#switchButton');
+const signInCont=document.querySelector('.SignIn');
+const signUpCont=document.querySelector('.Login');
+
+
+// Event Listener for Switch Button
+switchButton.addEventListener('change',()=>{
+    console.log('Switch Button Clicked');
+    
+    // display based on checked or not
+    if(switchButton.checked){
+        signInCont.style.display='none';
+        signUpCont.style.display='block';
+    }
+    else{
+        signInCont.style.display='block';
+        signUpCont.style.display='none';
+    }
+
 });
