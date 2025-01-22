7bca4457f2c0cad9e545c15d7b4c1e235c3e2004
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-01-22 14:26:41 -0500

    added more event listeners for user responsiveness

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,18 @@
-const signInUpButton=document.querySelector('#signInUpButton');
+import { createContact } from './createContact.js';
 
+
+
+const signInUpButton=document.querySelector('#signInUpButton');
+const closeButton1=document.querySelector('#closeButton1');
 signInUpButton.addEventListener('click',()=>{
     console.log('Button Clicked');
     document.querySelector('#overlay').style.display='block';
 });
 
+closeButton1.addEventListener('click',()=>{
+    document.querySelector('#overlay').style.display='none';
 
+});
 
 
 const switchButton=document.querySelector('#switchButton');
@@ -30,11 +37,38 @@ switchButton.addEventListener('change',()=>{
 });
 
 
-const addContactButton= document.querySelector('#addContactButton');
 
+
+
+
+
+
+const addContactButton= document.querySelector('#addContactButton');
+const closeButton2=document.querySelector('#closeButton2');
 addContactButton.addEventListener('click',()=>{
     console.log('Add Contact Button Clicked');
     document.querySelector('#overlay2').style.display='block';
+});
+
+closeButton2.addEventListener('click',()=>{
+    document.querySelector('#overlay2').style.display='none';
+});
+
+
+
+const addSubmitButton=document.querySelector('#addSubmitButton');
+
+addSubmitButton.addEventListener('click',(event)=>{
+    event.preventDefault();
+
+    createContact('John','Doe','123-456-7890','email');
+
+    // issue with FormData object
+    
+    // var formData = new FormData(document.querySelector('#addContactForm'));
+    
+    // createContact(formData.get('firstName'),formData.get('lastName'),formData.get('phone'),formData.get('email'));
+    //add code here to send to API endpoints
 });
 
 
